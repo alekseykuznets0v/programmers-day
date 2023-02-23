fe90625a39e42229f2e94def029920213be324a3
Aleksey Kuznetsov <teaguest@yandex.ru>
2023-02-23 03:28:30 +0300

    ok

---
+++ src/Praktikum.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Praktikum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите год:");
+        int year = scanner.nextInt();
+
+        if (isLeapYear(year)) {
+            System.out.println("День программиста будет 12.09." + year);// здесь нужно вывести результат
+        } else {
+            System.out.println("День программиста будет 13.09." + year);
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        } else if (year % 100 == 0){
+            return false;// здесь нужно определить, является ли переданный год високосным
+        } else if (year % 4 == 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
